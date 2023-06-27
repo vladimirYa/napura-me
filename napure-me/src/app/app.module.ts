ff1748b875f0373534e45914c7ec01d74e110641
ADA7SZZ_mmm <vladimiryaryhin@Vladimirs-MacBook-Pro.local>
2023-06-27 19:59:36 +0200

    about page

--- napure-me/src/app/app.module.ts
+++ napure-me/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing.module';
 import { NgxTranslateModule } from './modules/translate/translate.module';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, RouterModule, AppRoutingModule, NgxTranslateModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomeComponent, AboutComponent],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        AppRoutingModule,
+        NgxTranslateModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
--- napure-me/src/app/components/pages/home/home.component.ts
+++ napure-me/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+interface HeaderItem {
+    id: string;
+    labelKey: string;
+    isActive: boolean;
+}
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
@@ -10,11 +16,32 @@ export class HomeComponent implements OnInit {
     langs: string[] = ['cg', 'en', 'ru'];
     currentLang: string = 'en';
 
-    headerItems = [
-        'header.about',
-        'header.catalog',
-        'header.becomeAMaster',
-        'header.wholesale',
+    headerItems: HeaderItem[] = [
+        {
+            id: 'home',
+            labelKey: 'header.home',
+            isActive: true,
+        },
+        {
+            id: 'about',
+            labelKey: 'header.about',
+            isActive: false,
+        },
+        {
+            id: 'catalog',
+            labelKey: 'header.catalog',
+            isActive: false,
+        },
+        {
+            id: 'master',
+            labelKey: 'header.becomeAMaster',
+            isActive: false,
+        },
+        {
+            id: 'wholesale',
+            labelKey: 'header.wholesale',
+            isActive: false,
+        },
     ];
     constructor(private translate: TranslateService) {}
 
@@ -22,10 +49,31 @@ export class HomeComponent implements OnInit {
         this.translate.setDefaultLang('en');
         this.translate.addLangs(this.langs);
         this.translate.use('en');
+        this.syncTabs('home');
     }
 
     selectLang(lang: string): void {
         this.translate.use(lang);
         this.currentLang = lang;
+    }
+
+    selectTab(id: string): void {
+        this.headerItems.forEach((item: HeaderItem) => {
+            item.isActive = item.id === id;
+        });
+
+        this.syncTabs(id);
+    }
+
+    syncTabs(id: string) {
+        const tabs = document.querySelectorAll('[data-tab]');
+
+        tabs.forEach((tab) => {
+            if (tab.attributes['data-tab' as any].nodeValue === id) {
+                tab.classList.add('fadeIn');
+            } else {
+                tab.classList.remove('fadeIn');
+            }
+        });
     }
 }
